8264ba555bb36bed63dc0022a223a7725171c92d
Daniel Sávio <daniel.savio@treetech.com.br>
2024-03-26 17:52:38 -0300

    First commit

---
+++ app/controllers/users_controller.ts
@@ -0,0 +1,29 @@
+import User from '#models/user'
+import type { HttpContext } from '@adonisjs/core/http'
+
+export default class UsersController {
+  /**
+   *Controller for creating a user
+   */
+
+  async create({ response, request, auth }: HttpContext) {
+    await auth.authenticate()
+    console.log(await request)
+    const newUser = await request.only(['fullName', 'email', 'password', 'isAdmin'])
+    try {
+      const user = await User.create(newUser)
+      return user.fullName, user.email
+    } catch (err) {
+      response.status(404).send('Invalid information')
+    }
+  }
+
+  /**
+   *Controller for listing users but with authentication
+   */
+  async getUsers({ response, auth }: HttpContext) {
+    const user: User = await auth.authenticate()
+
+    response.send({ user: user.fullName, userList: User.all() })
+  }
+}
